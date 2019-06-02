bad1cc08b58c70ef0f4e9ab4f8cc9f4ae3d39256
MendezBruno <brunomendez88@hotmail.com>
2019-06-02 17:57:44 -0300

    server terminado

--- app.py
+++ app.py
@@ -4,21 +4,42 @@ import requests
 
 app = Flask(__name__)
 CORS(app)
+token = ""
+
+from requests.auth import AuthBase
+
+class TokenAuth(AuthBase):
+    """Implements a custom authentication scheme."""
+
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        """Attach an API token to a custom auth header."""
+        r.headers['X-TokenAuth'] = f'{self.token}'  # Python 3.6+
+        return r
 
 
 @app.route("/")
 def hello():
-    return jsonify({'text':'Hello World!'})
+    return jsonify({'text': 'Hello World!'})
+
 
 @app.route("/user/signin/callback")
 def callbackGitHub():
+    global token
+    token = request.args['code']
     return jsonify(request.args)
 
-@app.route("/users")
-def getUser():
-    r = requests.get('https://github.com/users')
-    return r.status_code
+
+@app.route("/users/<username>")
+def getUser(username):
+    #requests.get('https://httpbin.org/get', auth=TokenAuth('12345abcde-token'))
+    r = requests.get('https://api.github.com/search/users',
+    params={'q': username},
+    auth=TokenAuth(token))
+    return jsonify(r.text)
 
 
 if __name__ == '__main__':
-     app.run(host="0.0.0.0",port=5000,debug=True,use_reloader=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
